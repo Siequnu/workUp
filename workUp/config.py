b14b4365d2c0d520aaacf2d41068bf458a8666fc
Siequnu <pleif@me.com>
2018-02-23 22:42:01 +0800

    Security changes: add config file with dummy passwords and enable password protection for the whole server

---
+++ workUp/config.py
@@ -0,0 +1,5 @@
+# This is a configuration file. This should be changed and populated with secrets on the server
+
+BASIC_AUTH_USERNAME = 'john'
+BASIC_AUTH_PASSWORD = 'matrix'
+app_secret_key = 'ignitors0gitanas*vapours3'
