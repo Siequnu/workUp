042df96185b162f7898f6500af80e3bbb6764ef0
Siequnu <pleif@me.com>
2018-02-24 10:11:37 +0800

    Change security from BasicAuth to HTTPBasicAuth

--- workUp/config.py
+++ workUp/config.py
@@ -2,6 +2,5 @@
 
 #!!!This should be changed and populated with secrets on each server#!!!
 
-BASIC_AUTH_USERNAME = 'john'
-BASIC_AUTH_PASSWORD = 'matrix'
 app_secret_key = 'ignitors0gitanas*vapours3'
+
