5cb5abf9e4086664ae67abed7ccad19dc4ed89ee
Siequnu <11821606+Siequnu@users.noreply.github.com>
2018-02-24 20:19:46 +0800

    Log-in: Add SQL database backend with hashed passwords, register screen

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
 from config import Config
 
+#SQL
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 workUpApp = Flask(__name__)
 workUpApp.config.from_object(Config)
 
-from app import views
+# SQL
+db = SQLAlchemy(workUpApp)
+migrate = Migrate(workUpApp, db)
+
+login = LoginManager(workUpApp)
+login.login_view = 'login'
+
+from app import views, models
+
