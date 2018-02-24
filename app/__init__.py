3a735b6d4da9b74e46bffed897fc292912860611
Siequnu <11821606+Siequnu@users.noreply.github.com>
2018-02-24 22:28:41 +0800

    Logging: Make filepath to log explicit

--- app/__init__.py
+++ app/__init__.py
@@ -30,17 +30,17 @@ bootstrap = Bootstrap(workUpApp)
 
 # Log errors to local log
 if not workUpApp.debug:
-    if not os.path.exists(os.path.join(workUpApp.config['APP_ROOT'], 'logs')):
-        os.mkdir(os.path.join(workUpApp.config['APP_ROOT'], 'logs'))
-    file_handler = RotatingFileHandler('logs/workUpApp.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
+	logsPath = os.path.join(workUpApp.config['APP_ROOT'], 'logs')
+	if not os.path.exists(os.path.join(logsPath)):
+		os.mkdir(logsPath)
+	file_handler = RotatingFileHandler(os.path.join(logsPath, 'workUpApp.log'), maxBytes=10240, backupCount=10)
+	file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    workUpApp.logger.addHandler(file_handler)
-
-    workUpApp.logger.setLevel(logging.INFO)
-    workUpApp.logger.info('workUpApp startup')
+	file_handler.setLevel(logging.INFO)
+	workUpApp.logger.addHandler(file_handler)
+	
+	workUpApp.logger.setLevel(logging.INFO)
+	workUpApp.logger.info('workUpApp startup')
 
 from app import views, models, errors
 
