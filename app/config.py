99525a6f596ce0b897b87f77d12d070feb1b9bd9
Siequnu <pleif@me.com>
2018-02-24 20:19:46 +0800

    Log-in: Add SQL database backend with hashed passwords, register screen

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(12) or 'ignitors0gitanas*vapours3'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(12) or \
+		'ignitors0gitanas*vapours3'
 	MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 	APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 	UPLOAD_LOCATION = 'static/uploads'
@@ -12,3 +14,8 @@ class Config(object):
 		"susan": "bye"
 	}
 	ADMIN_USERS = {'john'}
+	
+	# SQL
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'workUpApp.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/views.py
+++ app/views.py
@@ -1,13 +1,24 @@
 from app import workUpApp
 
-from flask import render_template, request, Response, make_response, send_file, redirect, url_for, send_from_directory, flash, abort
+from flask import render_template, session, request, Response, make_response, send_file, redirect, url_for, send_from_directory, flash, abort
 from flask_httpauth import HTTPBasicAuth
 from random import randint
 from werkzeug import secure_filename
 import glob, os
 
+## SQL
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
+from flask_login import login_required
+from werkzeug.urls import url_parse
+from app import db
+from app.forms import RegistrationForm
+
+
+# Personal classes
 import upDownTools
-from app.login import LoginForm
+from app.forms import LoginForm
 
 # App security for stageing server
 auth = HTTPBasicAuth()
@@ -18,16 +29,44 @@ def get_pw(username):
         return workUpApp.config['USERS'].get(username)
     return None
 
+# Log-out page
+@workUpApp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# Registration
+@workUpApp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 # Log-in page
 @workUpApp.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('uploadFile'))
-    return render_template('login.html', form=form)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
 
 # Choose a random file from uploads folder and send it out for download
 @workUpApp.route('/downloadPeerFile', methods=['POST'])
@@ -54,7 +93,8 @@ def index():
 
 # Upload form
 @workUpApp.route('/upload', methods=['GET', 'POST'])
-@auth.login_required
+#@auth.login_required
+@login_required
 def uploadFile():
 	# If the form has been filled out and posted:
 	if request.method == 'POST':
@@ -71,13 +111,13 @@ def uploadFile():
 			file.save(os.path.join(workUpApp.config['UPLOAD_FOLDER'], filename))
 			return redirect(url_for('uploadedFile',filename=filename))
 	else:
-		flash('Hello, ' + str(auth.username()) + '!')
 		return render_template('fileUpload.html')
 
 
 # Access file stats
 @workUpApp.route("/fileStats")
-@auth.login_required
+#@auth.login_required
+@login_required
 def fileStats():
 	if (str(auth.username()) in workUpApp.config['ADMIN_USERS'] and workUpApp.config['USERS']):
 		printOutput = 'There are ' + str(upDownTools.getNumberOfFiles()) +" files in the folder: "
