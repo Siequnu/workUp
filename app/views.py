216e29395d0502fbbe2ece8bfd0e795d933aee0a
Siequnu <pleif@me.com>
2018-02-24 14:17:21 +0800

    Rename app to workUpApp for better distinction with flask app

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from app import app
+from app import workUpApp
 
 from flask import render_template, request, Response, make_response, send_file, redirect, url_for, send_from_directory, flash, abort
 from flask_httpauth import HTTPBasicAuth
@@ -13,8 +13,8 @@ auth = HTTPBasicAuth()
 
 @auth.get_password
 def get_pw(username):
-    if username in app.config['USERS']:
-        return app.config['USERS'].get(username)
+    if username in workUpApp.config['USERS']:
+        return workUpApp.config['USERS'].get(username)
     return None
 
 '''
@@ -25,22 +25,22 @@ def login():
     #return render_template('login.html', title='Sign In', form=form)
 '''
 # Choose a random file from uploads folder and send it out for download
-@app.route('/download-peer-file', methods = ['GET', 'POST'])
+@workUpApp.route('/download-peer-file', methods = ['GET', 'POST'])
 def downloadRandomFile():	
-   uploadedFiles = (os.listdir(app.config['UPLOAD_FOLDER']))
+   uploadedFiles = (os.listdir(workUpApp.config['UPLOAD_FOLDER']))
    numberOfFiles = int (upDownTools.getNumberOfFiles())
    randomNumber = (randint(0,numberOfFiles - 1))
-   randomFile = os.path.join (app.config['UPLOAD_LOCATION'], uploadedFiles[randomNumber])
+   randomFile = os.path.join (workUpApp.config['UPLOAD_LOCATION'], uploadedFiles[randomNumber])
    return send_file(randomFile, as_attachment=True)
 
 # Sends out a file for download
 # Input: filename (must be in upload folder)
-@app.route('/uploaded/<filename>')
+@workUpApp.route('/uploaded/<filename>')
 def uploaded_file(filename):
 	return render_template ('fileUploaded.html')
 
 # Main entrance to the app
-@app.route('/', methods=['GET', 'POST'])
+@workUpApp.route('/', methods=['GET', 'POST'])
 @auth.login_required
 def upload_file():
 	# If the form has been filled out and posted:
@@ -55,19 +55,19 @@ def upload_file():
 			return redirect(request.url)
 		if file and upDownTools.allowedFile(file.filename):
 			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			file.save(os.path.join(workUpApp.config['UPLOAD_FOLDER'], filename))
 			return redirect(url_for('uploaded_file',filename=filename))
 	else:
 		flash('Hello, ' + str(auth.username()) + '!')
 		return render_template('fileUpload.html')
 
 # Access file stats
-@app.route("/fileStats")
+@workUpApp.route("/fileStats")
 @auth.login_required
 def fileStats():
-	if (str(auth.username()) in app.config['ADMIN_USERS'] and app.config['USERS']):
+	if (str(auth.username()) in workUpApp.config['ADMIN_USERS'] and workUpApp.config['USERS']):
 		printOutput = 'There are ' + str(upDownTools.getNumberOfFiles()) +" files in the folder: "
-		uploadedFiles = (glob.glob(app.config['UPLOAD_FOLDER'] + '/*'))
+		uploadedFiles = (glob.glob(workUpApp.config['UPLOAD_FOLDER'] + '/*'))
 		return render_template('fileStats.html', numberOfFiles = str(upDownTools.getNumberOfFiles()), uploadedFileNamesArray = uploadedFiles)
 	abort(403)
 	return None
