ba1414852427a45ec3ff2234a4baa2747a9e5f9b
Siequnu <pleif@me.com>
2018-02-25 01:16:13 +0800

    File Uploads: Add database registration and random file names

--- app/upDownTools.py
+++ app/upDownTools.py
@@ -7,6 +7,9 @@ def allowedFile(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in workUpApp.config['ALLOWED_EXTENSIONS']
 
+def getFileExtension(filename):
+	return filename.rsplit('.', 1)[1].lower()
+
 # Return the number of files in the upload folder
 def getNumberOfFiles():
 	return (len (os.listdir(workUpApp.config['UPLOAD_FOLDER'])) - 1 )
