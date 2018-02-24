3584e1dee1245866e67af8692b7199c2034ced55
Siequnu <11821606+Siequnu@users.noreply.github.com>
2018-02-24 22:10:13 +0800

    Errors: Add error logging

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,11 @@ from flask_login import LoginManager
 # Bootstrap
 from flask_bootstrap import Bootstrap
 
+# Logging handlers
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 workUpApp = Flask(__name__)
 workUpApp.config.from_object(Config)
 
@@ -22,6 +27,20 @@ login.login_view = 'login'
 
 # Bootstrap
 bootstrap = Bootstrap(workUpApp)
+
+# Log errors to local log
+if not workUpApp.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/workUpApp.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    workUpApp.logger.addHandler(file_handler)
+
+    workUpApp.logger.setLevel(logging.INFO)
+    workUpApp.logger.info('workUpApp startup')
 
 from app import views, models, errors
 
