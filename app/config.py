be48eb55fdb1ed20b2a6e0d30965713e948d4e1e
Siequnu <pleif@me.com>
2018-02-24 22:50:37 +0800

    Config: add sample configuration

--- app/config.py
+++ app/config.py
@@ -2,14 +2,13 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(12) or \
-		'ignitors0gitanas*vapours3'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(12)
 	MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 	APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 	UPLOAD_LOCATION = 'static/uploads'
 	UPLOAD_FOLDER = os.path.join(APP_ROOT, UPLOAD_LOCATION)
 	ALLOWED_EXTENSIONS = set(['txt', 'zip', 'pdf', 'doc', 'docx', 'pages'])
-	ADMIN_USERS = {'siequnu'}
+	ADMIN_USERS = {''}
 	
 	# SQL
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
