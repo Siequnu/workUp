0dcdc6a235f2510e4bde40442d8a6a6146f5fe65
Siequnu <11821606+Siequnu@users.noreply.github.com>
2018-02-24 21:14:57 +0800

    Templates: add flask-bootstrap for theme

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Config
 
-#SQL
+# SQL
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+
+# Bootstrap
+from flask_bootstrap import Bootstrap
 
 workUpApp = Flask(__name__)
 workUpApp.config.from_object(Config)
@@ -13,8 +16,12 @@ workUpApp.config.from_object(Config)
 db = SQLAlchemy(workUpApp)
 migrate = Migrate(workUpApp, db)
 
+# Log-in
 login = LoginManager(workUpApp)
 login.login_view = 'login'
+
+# Bootstrap
+bootstrap = Bootstrap(workUpApp)
 
 from app import views, models
 
