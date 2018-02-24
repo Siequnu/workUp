8e732bc80dfbccb0b351b560fcd021e05cff4e1b
Siequnu <pleif@me.com>
2018-02-24 18:31:31 +0800

    Config: secret key generated randomly

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'ignitors0gitanas*vapours3'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(12) or 'ignitors0gitanas*vapours3'
 	MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 	APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 	UPLOAD_LOCATION = 'static/uploads'
