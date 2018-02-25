b58a2170a49b7041897eb0c8c45da5f47b803efa
Siequnu <11821606+Siequnu@users.noreply.github.com>
2018-02-25 13:54:21 +0800

    Change DB handling of user downloads

--- app/models.py
+++ app/models.py
@@ -12,8 +12,7 @@ class User(UserMixin, db.Model):
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
-	studentnumber = db.Column(db.Integer)
-	posts = db.relationship('Post', backref='author', lazy='dynamic')
+	studentnumber = db.Column(db.String(12))
 	last_seen = db.Column(db.DateTime, default=datetime.now)
 	registered = db.Column(db.DateTime, default=datetime.now)
 
