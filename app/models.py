131b46101ed50353775785ae89fd87444a6ea477
Siequnu <11821606+Siequnu@users.noreply.github.com>
2018-02-25 16:40:31 +0800

    Models: Update representation of Post and Download

--- app/models.py
+++ app/models.py
@@ -33,7 +33,7 @@ class Post(db.Model):
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 	
 	def __repr__(self):
-		return '<Post {}>'.format(self.body)
+		return '<Post {}>'.format(self.filename)
 	
 class Download(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -42,4 +42,4 @@ class Download(db.Model):
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 	
 	def __repr__(self):
-		return '<Post {}>'.format(self.body)
+		return '<Post {}>'.format(self.filename)
