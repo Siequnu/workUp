a305ef919ed591fa123327ecaab7fac5547365bb
Siequnu <pleif@me.com>
2018-02-25 09:34:31 +0800

    Model registers file download info to DB

--- app/models.py
+++ app/models.py
@@ -34,3 +34,12 @@ class Post(db.Model):
 	
 	def __repr__(self):
 		return '<Post {}>'.format(self.body)
+	
+class Download(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	filename = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	
+	def __repr__(self):
+		return '<Post {}>'.format(self.body)
