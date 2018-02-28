c6465b73077d40729e0e1e31cbd9454813a9faaf
Siequnu <11821606+Siequnu@users.noreply.github.com>
2018-02-28 12:34:50 +0800

    Registration: Form asks for student class ID

--- app/forms.py
+++ app/forms.py
@@ -1,30 +1,35 @@
+from app import workUpApp
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 
+
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
+	username = StringField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember_me = BooleanField('Remember me')
+	submit = SubmitField('Sign In')
 	
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
-    studentNumber = StringField('Student number', validators=[DataRequired()])
-    signUpCode = StringField('Sign-up code', validators=[DataRequired()])
-    submit = SubmitField('Register')
+	username = StringField('Username', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
+	studentNumber = StringField('Student number', validators=[DataRequired()])
+	classChoices = workUpApp.config['CLASS_CHOICES']
+	classId = SelectField('Class ID', choices=classChoices, validators=[DataRequired()])
+	signUpCode = StringField('Sign-up code', validators=[DataRequired()])
+	submit = SubmitField('Register')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise ValidationError('Please use a different username.')
 
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different email address.')
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError('Please use a different email address.')
