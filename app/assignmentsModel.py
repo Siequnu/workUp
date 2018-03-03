d8cee84a66095d675666eb4c1279bc0f4c165720
Siequnu <pleif@me.com>
2018-03-03 11:09:47 +0800

    viewAssignments: Due date formatted without time

--- app/assignmentsModel.py
+++ app/assignmentsModel.py
@@ -3,8 +3,21 @@ from app.models import Post, Download, Assignment, User
 import datetime
 
 def getAllAssignments ():
-	return Assignment.getAllAssignments()
+	assignments = Assignment.getAllAssignments()
 	# [(1, u'title', u'descrip', u'2018-03-02 00:00:00.000000', 1, u'30640192-1', u'2018-02-28 13:05:59.287555')]
+	cleanAssignmentsArray = []
+	for assignment in assignments:
+		cleanAssignment = {} 
+		cleanAssignment['assignmentId'] = assignment[0]
+		cleanAssignment['assignmentTitle'] = assignment[1]
+		cleanAssignment['assignmentDescription'] = assignment[2]
+		cleanAssignment['assignmentDue'] = datetime.datetime.strptime(assignment[3], '%Y-%m-%d %H:%M:%S.%f').date()
+		cleanAssignment['assignmentCreatedBy'] = assignment[4]
+		cleanAssignment['assignmentForClassId'] = assignment[5]
+		cleanAssignment['assignmentCreationTimestamp'] = assignment[6]
+		cleanAssignmentsArray.append(cleanAssignment)
+	
+	return cleanAssignmentsArray
 	
 def getUserClassFromId (userId):
 	return User.getUserClassFromId(userId)
