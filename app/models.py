fc944b81382c0cc32eb21baa4654cc7912b4c213
Siequnu <pleif@me.com>
2018-03-03 11:18:36 +0800

    Index: remove personal information from index page

--- app/models.py
+++ app/models.py
@@ -154,7 +154,7 @@ class Assignment(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(140))
 	description = db.Column(db.String(280))
-	due_date = db.Column(db.DateTime, default=datetime.now)
+	due_date = db.Column(db.Date)
 	created_by_id = db.Column(db.Integer)
 	target_course = db.Column(db.String(120))
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
