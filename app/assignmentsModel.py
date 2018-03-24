a68ed36c7899c626d2c57a6c1d89b8b5ee9fd6c0
Patrick Johansson <patrick@Air.local>
2018-03-24 08:33:29 +0800

    Views: Move progress bar logic to assignmentsModel

--- app/assignmentsModel.py
+++ app/assignmentsModel.py
@@ -5,6 +5,7 @@ import datetime
 from datetime import datetime
 import time
 import string
+from flask_login import current_user
 
 def getAllAssignments ():
 	assignments = app.models.selectFromDb(['*'], 'assignment')
@@ -96,3 +97,54 @@ def getUserAssignmentInformation (userId):
 		cleanAssignmentsArray.append(cleanAssignment)
 	
 	return cleanAssignmentsArray
+
+def getAssignmentUploadProgressPercentage ():
+	# Get turmaId for this user
+	turmaId = getUserTurmaFromId (current_user.id)
+	
+	# Get assignments due for this user
+	assignmentsInfo = getAssignmentsFromTurmaId(turmaId)
+	assignmentsForThisUser = []
+	for assignment in assignmentsInfo:
+		assignmentId = str(assignment[0])
+		assignmentsForThisUser.append(assignmentId)
+	
+	# Check if user has uploaded each assignment
+	completedAssignments = 0
+	for assignmentId in assignmentsForThisUser:
+		userUploadedAssignmentId = Assignment.getUsersUploadedAssignmentsFromAssignmentId (assignmentId, current_user.id)
+		if userUploadedAssignmentId:
+			completedAssignments += 1
+	
+	# Set value of assignment upload progress bar
+	if (len(assignmentsForThisUser) != 0):
+		assignmentUploadProgressBarPercentage = int(float(completedAssignments)/float(len(assignmentsForThisUser)) * 100)
+	else:
+		assignmentUploadProgressBarPercentage = 100
+	
+	return assignmentUploadProgressBarPercentage
+	
+def getPeerReviewProgressPercentage ():
+	# Get turmaId for this user
+	turmaId = getUserTurmaFromId (current_user.id)
+	
+	# Get assignments due for this user
+	assignmentsInfo = getAssignmentsFromTurmaId(turmaId)
+	assignmentsForThisUser = []
+	for assignment in assignmentsInfo:
+		assignmentId = str(assignment[0])
+		assignmentsForThisUser.append(assignmentId)
+	
+	totalNumberOfPeerReviewsExpected = (len(assignmentsForThisUser)) * 2 # At two peer reviews per assignment
+	conditions = []
+	conditions.append(string.join(('user_id=', str(current_user.id)), ''))
+	conditions.append('pending=0')
+	commentIds = app.models.selectFromDb(['id'], 'comment', conditions)
+	totalNumberOfCompletedPeerReviews = (len(commentIds))
+	
+	if totalNumberOfPeerReviewsExpected != 0:
+		peerReviewProgressBarPercentage = int(float(totalNumberOfCompletedPeerReviews)/float(totalNumberOfPeerReviewsExpected) * 100)
+	else:
+		peerReviewProgressBarPercentage = 100
+		
+	return peerReviewProgressBarPercentage
