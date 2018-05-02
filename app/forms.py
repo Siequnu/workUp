a5602e1611cf992a4d4b3666ca525f8877723c1a
Siequnu <11821606+Siequnu@users.noreply.github.com>
2018-05-02 11:32:28 +0800

    Forms: Remove redundant forms

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,12 @@ from app import workUpApp
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, DateField, RadioField, FormField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+
 from app.models import User, Turma
+
+# Import user defined peer review forms
+from peerReviewForms import *
+
 
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
@@ -70,56 +75,9 @@ class TurmaCreationForm(FlaskForm):
 	turmaTerm = StringField('Class term', validators=[DataRequired()])
 	turmaYear = StringField('Class year', validators=[DataRequired()])
 	submit = SubmitField('Create')
+
+class FormModel (FlaskForm):
+	pass
 	
 	
-class PeerReviewForm(FlaskForm):
-	agreeDisagreeFivePartLikertScale = [('stronglyagree', 'Strongly agree'), ('agree', 'Agree'), ('neutral', 'Neutral'), ('disagree', 'Disagree'), ('stronglydisagree', 'Strongly disagree')]
-	wordCountLabel= 'Did this piece meet the word limit requirement of approximately 500 words? (10% over is acceptable)'
-	wordCount = RadioField(label=wordCountLabel, choices=agreeDisagreeFivePartLikertScale)
 	
-	essayRequirementLabel = "Did this piece meet the requirement of an engagement with at least three academic sources?"
-	essayRequirementRadio = RadioField(label=essayRequirementLabel, choices=agreeDisagreeFivePartLikertScale)
-	essayRequirementDescription = StringField('Comment and justification:', validators=[DataRequired()])
-	
-	essayRequirementLabel = "Did the essay critically engage with the concepts and theories covered in the course?"
-	criticallyEngageRadio = RadioField(label=essayRequirementLabel, choices=agreeDisagreeFivePartLikertScale)
-	criticallyEngageDescription = StringField('Comment and justification:', validators=[DataRequired()])
-	
-	essayRequirementLabel = "Has the author convincingly applied theoretical concepts from the course to screen media texts to thoroughly develop their argument? Or is the work is derivative of theoretical sources? Does the citation of more than one screen text impair the adequate development of the author's argument?"
-	theoreticallyAppliedRadio = RadioField(label=essayRequirementLabel, choices=agreeDisagreeFivePartLikertScale)
-	theoreticallyAppliedDescription = StringField('Comment and justification:', validators=[DataRequired()])
-	
-	essayRequirementLabel = "Was the writer's tone engaging in this piece?"
-	writersToneRadio = RadioField(label=essayRequirementLabel, choices=agreeDisagreeFivePartLikertScale)
-	writersToneDescription = StringField('Comment and justification:', validators=[DataRequired()])
-	
-	essayRequirementLabel = "Does this piece display innovative or original thought?"
-	innovativeThoughtRadio = RadioField(label=essayRequirementLabel, choices=agreeDisagreeFivePartLikertScale)
-	innovativeThoughtDescription = StringField('Comment and justification:', validators=[DataRequired()])
-	
-	essayRequirementLabel = "Was this piece stimulating and/or insightful?"
-	stimatulatingPieceRadio = RadioField(label=essayRequirementLabel, choices=agreeDisagreeFivePartLikertScale)
-	stimulatingPieceDescription = StringField('Comment and justification:', validators=[DataRequired()])
-	
-	furtherSuggestionsDescription = StringField('Do you have any suggestions for how this author can improve on this piece or develop it further for the final research essay? Have you got any suggestions that you believe might assist the author in general?', validators=[DataRequired()])
-	submit = SubmitField('Submit')
-	
-class PeerReviewFormTwo (FlaskForm):
-	
-	agreeDisagreeFivePartLikertScale = [('stronglyagree', 'Strongly agree'), ('agree', 'Agree'), ('neutral', 'Neutral'), ('disagree', 'Disagree'), ('stronglydisagree', 'Strongly disagree')]
-	essayRequirementLabel = 'Identify values in the paper (What did you like? What are the best parts? What are the strongest points? What surprised you?'
-	valuesPaperField = TextAreaField(label=essayRequirementLabel, validators=[DataRequired()])
-	
-	essayRequirementLabel = "Describe the form of the paper (structure, organisation, surface errors, etc...)"
-	formDescriptionField = TextAreaField(label=essayRequirementLabel, validators=[DataRequired()])
-	
-	essayRequirementLabel = "Describe the content of the paper (topic, meaning... Did it answer the prompt given?"
-	contentDescriptionField = TextAreaField(label=essayRequirementLabel, validators=[DataRequired()])
-	
-	essayRequirementLabel = "Ask questions about the paper (What needs clarifying? What did you mean when you said...)"
-	questionsAboutPaperField = TextAreaField(label=essayRequirementLabel, validators=[DataRequired()])
-	
-	essayRequirementLabel = "Suggest some points for the author to improve"
-	pointsToImproveField = TextAreaField(label=essayRequirementLabel, validators=[DataRequired()])
-	
-	submit = SubmitField('Submit')
