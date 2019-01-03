dbbe806a0ba33186b19694fca6197b7cb44b7c00
Siequnu <11821606+Siequnu@users.noreply.github.com>
2019-01-03 12:17:27 +0800

    Move user forms to user/forms blueprint

--- app/user/routes.py
+++ app/user/routes.py
@@ -20,17 +20,14 @@ import app.util
 
 # Forms
 import app.forms
-from app.forms import RegistrationForm, AdminRegistrationForm, EmailForm , PasswordForm, LoginForm
-
 from app.user import bp
-
 
 # Log-in page
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
 	if current_user.is_authenticated:
 		return redirect(url_for('index'))
-	form = LoginForm()
+	form = app.user.forms.LoginForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(username=form.username.data).first()
 		if user is None or not user.check_password(form.password.data):
@@ -59,33 +56,12 @@ def logout():
 
 
 # Registration
-@bp.route('/registeradmin', methods=['GET', 'POST'])
-def registerAdmin():
-	if current_user.is_authenticated:
-		return redirect(url_for('index'))
-	form = AdminRegistrationForm()
-	if form.validate_on_submit():
-		if form.signUpCode.data in workUpApp.config['ADMIN_SIGNUP_CODES']:
-			user = User(username=form.username.data, email=form.email.data)
-			user.set_password(form.password.data)
-			db.session.add(user)
-			db.session.commit()
-			flash('Congratulations, you are now a registered administrator!')
-			return redirect(url_for('user.login'))
-		else:
-			flash("Admin sign-up failed.")
-			return redirect(url_for('user.login'))
-	return render_template('user/register.html', title='Register', form=form)
-
-
-
-# Registration
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
 	if current_user.is_authenticated:
 		return redirect(url_for('index'))
 	if workUpApp.config['REGISTRATION_IS_OPEN'] == True:
-		form = RegistrationForm()
+		form = app.user.forms.RegistrationForm()
 		if form.validate_on_submit():
 			if form.signUpCode.data in workUpApp.config['SIGNUP_CODES']:
 				user = User(username=form.username.data, email=form.email.data, studentnumber=form.studentNumber.data, turma_id=form.turmaId.data)
@@ -95,10 +71,10 @@ def register():
 				
 				# Send the email confirmation link
 				subject = "Confirm your email"
-				token = util.ts.dumps(str(form.email.data), salt=workUpApp.config["TS_SALT"])
+				token = app.util.ts.dumps(str(form.email.data), salt=workUpApp.config["TS_SALT"])
 				confirm_url = url_for('user.confirm_email', token=token, _external=True)
 				html = render_template('email/activate.html',confirm_url=confirm_url)
-				util.sendEmail (user.email, subject, html)
+				app.util.sendEmail (user.email, subject, html)
 				
 				flash('Congratulations, you are now a registered user! Please confirm your email.')
 				return redirect(url_for('user.login'))
@@ -116,7 +92,7 @@ def register():
 @bp.route('/confirm/<token>')
 def confirm_email(token):
 	try:
-		email = util.ts.loads(token, salt=workUpApp.config["TS_SALT"], max_age=86400)
+		email = app.util.ts.loads(token, salt=workUpApp.config["TS_SALT"], max_age=86400)
 	except:
 		abort(404)
 	user = User.query.filter_by(email=email).first_or_404()
@@ -131,16 +107,16 @@ def confirm_email(token):
 # Reset password form
 @bp.route('/reset', methods=["GET", "POST"])
 def reset():
-	form = EmailForm()
+	form = app.user.forms.EmailForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(email=form.email.data).first_or_404()
 		subject = "Password reset requested"
-		token = util.ts.dumps(user.email, salt=workUpApp.config["TS_RECOVER_SALT"])
+		token = app.util.ts.dumps(user.email, salt=workUpApp.config["TS_RECOVER_SALT"])
 
 		recover_url = url_for('user.reset_with_token', token=token, _external=True)
 		html = render_template('email/recover.html', recover_url=recover_url)
 		
-		util.sendEmail(user.email, subject, html)
+		app.util.sendEmail(user.email, subject, html)
 		flash('An email has been sent to your inbox with a link to recover your password.')
 		return redirect(url_for('index'))
 		
@@ -152,10 +128,10 @@ def reset():
 @bp.route('/reset/<token>', methods=["GET", "POST"])
 def reset_with_token(token):
 	try:
-		email = util.ts.loads(token, salt=workUpApp.config['TS_RECOVER_SALT'], max_age=workUpApp.config['TS_MAX_AGE'])
+		email = app.util.ts.loads(token, salt=workUpApp.config['TS_RECOVER_SALT'], max_age=workUpApp.config['TS_MAX_AGE'])
 	except:
 		abort(404)
-	form = PasswordForm()
+	form = app.user.forms.PasswordForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(email=email).first_or_404()
 		user.set_password(form.password.data)
