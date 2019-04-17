b7630e1a3ecf96047a97f3416861a01d1fff858d
Siequnu <siequnu@siequnu.com>
2019-04-17 19:48:18 +0800

    Assignment creation: turma selection field is now a multiple choice field

--- app/assignments/forms.py
+++ app/assignments/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, DateField, RadioField, FormField, TextAreaField
+from wtforms import StringField, SubmitField, SelectField, SelectMultipleField, DateField, BooleanField, FormField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask_wtf.file import FileField, FileRequired
 from app.models import User, Turma
@@ -9,8 +9,7 @@ class AssignmentCreationForm(FlaskForm):
 	title = StringField('Assignment title', validators=[DataRequired()])
 	description = StringField('Assignment description', validators=[DataRequired()])
 	due_date = DateField('Due date:', validators=[DataRequired()])
-	# This should be a multiple choice field?
-	target_turma_id = SelectField('Class ID', choices=Turma.get_class_list_for_forms (), validators=[DataRequired()])
+	target_turma_id = SelectMultipleField('Class ID', choices=Turma.get_class_list_for_forms (), validators=[DataRequired()])
 	peer_review_necessary = BooleanField('Peer review necessary', default=True)
 	# Should be a SelectField, with default to BlankPeerReview
 	peer_review_form = StringField('Peer review form Class name', validators=[DataRequired()])
