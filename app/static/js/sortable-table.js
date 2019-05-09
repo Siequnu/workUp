6b7408ef578341f61ea04bef8afef5dec57e2827
Siequnu <siequnu@siequnu.com>
2019-05-09 15:39:02 +0800

    Don't filter table headers

--- app/static/js/sortable-table.js
+++ app/static/js/sortable-table.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     $('.btn-filter').on('click', function () {
       var $target = $(this).data('target');
       if ($target != 'all') {
-        $('.table tr').css('display', 'none');
-        $('.table tr[data-status="' + $target + '"]').fadeIn('slow');
+        $('#searchableTable tr').css('display', 'none');
+        $('#searchableTable tr[data-status="' + $target + '"]').fadeIn('slow');
       } else {
-        $('.table tr').css('display', 'none').fadeIn('slow');
+        $('#searchableTable tr').css('display', 'none').fadeIn('slow');
       }
     });
 
