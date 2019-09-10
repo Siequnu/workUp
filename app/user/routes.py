16a721ebe476c2182663f3da8a5da8b693e88880
Siequnu <11821606+Siequnu@users.noreply.github.com>
2019-09-10 13:15:03 +0800

    Fix bug not allowing teachers to sign up students

--- app/user/routes.py
+++ app/user/routes.py
@@ -62,7 +62,7 @@ def register():
 			
 		form.target_turmas.choices = [(turma.id, turma.turma_label) for turma in Turma.query.all()]
 		if form.validate_on_submit():
-			if form.signUpCode.data in current_app.config['SIGNUP_CODES'] or current_user.is_authenticated and app.models.is_admin(current_user.username):
+			if form.signUpCode and form.signUpCode.data in current_app.config['SIGNUP_CODES'] or current_user.is_authenticated and app.models.is_admin(current_user.username):
 				user = User(username=form.username.data, email=form.email.data, student_number=form.student_number.data, registered = datetime.now())
 				if current_user.is_authenticated is not True:
 					user.set_password(form.password.data)
