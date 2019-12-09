148f497d81e722a039ad77ba0aad4438c1f2f8c7
Siequnu <11821606+Siequnu@users.noreply.github.com>
2019-12-09 16:25:46 +0800

    Remove CKEditor from init

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,6 @@ from flask_toastr import Toastr
 from flask_compress import Compress
 import flask_excel as excel
 from flask_qrcode import QRcode
-from flask_ckeditor import CKEditor
 
 import logging, os
 from logging.handlers import RotatingFileHandler
