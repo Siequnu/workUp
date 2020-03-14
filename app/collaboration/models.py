c4f889e2ff5a95d59541ed4e9d69ba98ef2ab0f9
Siequnu <siequnu@siequnu.com>
2020-03-14 12:02:44 +0800

    Refactor collaboration route code into model class

--- app/collaboration/models.py
+++ app/collaboration/models.py
@@ -1,9 +1,40 @@
 from flask import current_app
 from flask_login import current_user
-
+from datetime import datetime
 from app import db
 from app.models import User, Firepad, Collab
 import app.models
+
+def get_user_owned_firepads():
+	firepads = Firepad.query.filter_by(owner_id=current_user.id).all()
+	firepads_info_array = []
+	for firepad in firepads:
+		firepad_dict = firepad.__dict__
+		firepad_dict['owner'] = app.collaboration.models.get_firepad_owner_user_object(firepad.id)
+		firepad_dict['collaborators'] = app.collaboration.models.get_firepad_collaborator_user_objects(firepad.id)
+		firepads_info_array.append(firepad_dict)
+	return firepads_info_array
+
+def get_user_collaborating_firepads():
+	collabs = Collab.query.filter_by(user_id=current_user.id).all()
+	collabs_info_array = []
+	for collab in collabs:
+		collab_dict = collab.__dict__
+		collab_dict['owner'] = app.collaboration.models.get_firepad_owner_user_object(collab.firepad_id)
+		collab_dict['collaborators'] = app.collaboration.models.get_firepad_collaborator_user_objects(collab.firepad_id)
+		collabs_info_array.append(collab_dict)
+	return collabs_info_array
+
+
+# Create a new firepad in the DB and return the object
+def create_new_firepad():
+	# Create a new firepad in the DB and redirect to the newly created pad
+	firepad = Firepad(owner_id=current_user.id, timestamp = datetime.now())
+	db.session.add(firepad)
+	db.session.flush() # Access the new firepad ID in the redirect
+	db.session.commit()
+	return firepad
+
 
 # Returns true if user is admin, owner, or collab in a firepad
 def check_if_user_has_access_to_firepad(firepad_id, user_id):
