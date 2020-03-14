bd2792106baec1106a5281fec9555a7ff838e023
Siequnu <11821606+Siequnu@users.noreply.github.com>
2020-03-14 10:28:53 +0800

    Add owner and colloborator information to collaboration index

--- app/collaboration/models.py
+++ app/collaboration/models.py
@@ -15,6 +15,20 @@ def check_if_user_has_access_to_firepad(firepad_id, user_id):
 		return True
 	else:
 		return False
+	
+def get_firepad_owner_user_object(firepad_id):
+	try:
+		return User.query.get(Firepad.query.get(firepad_id).owner_id)
+	except: return None
+
+
+def get_firepad_collaborator_user_objects(firepad_id):
+	try:
+		return db.session.query(
+			Collab, User).join(
+			User, Collab.user_id == User.id).filter(
+			Collab.firepad_id == firepad_id).all()
+	except: return None
 
 # Returns a list of collaborator user IDs 
 def check_if_user_is_a_collaborator (firepad_id, user_id):
