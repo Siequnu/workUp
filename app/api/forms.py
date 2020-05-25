86e16d37ba46cef6e89007ccfab1dffbaffb603f
Siequnu <11821606+Siequnu@users.noreply.github.com>
2020-05-25 12:21:01 +0800

    Remove API blueprint (preparation for submodule)

--- app/api/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, DateField, SelectMultipleField, BooleanField, FormField, TextAreaField, FileField
-from wtforms.validators import ValidationError, DataRequired
-from app import db
-	
-class ApiCreationForm(FlaskForm):
-	key = StringField('Api Key', validators=[DataRequired()])
-	description = StringField('Description', validators=[DataRequired()])
-	submit = SubmitField('Create API key')
--- app/api/schemas.py
+++
@@ -1,7 +0,0 @@
-from flask_marshmallow import Marshmallow
-from app.models import LibraryUpload
-from app import ma
-
-class LibraryUploadSchema (ma.SQLAlchemySchema):
-	class Meta:
-		fields = ('id', 'title', 'description', 'filename')
