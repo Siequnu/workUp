95e543a24322a861ae832753e77d1950e99648e5
Siequnu <11821606+Siequnu@users.noreply.github.com>
2020-06-09 16:56:38 +0800

    Add UK assets

---
+++ app/static/js/config_Unikey.js
@@ -0,0 +1,5 @@
+var config = {};
+	
+// Website API key
+config.apiKey = 'mQ4q27gNywtwMF6qpm0SHggptKfDS3UdYA_ZGqTb4aiU5sDu7-2Zhw';
+
---
+++ unikey_portal.py
@@ -0,0 +1,3 @@
+from app import create_app
+from config import Unikey
+app = create_app(Unikey)
