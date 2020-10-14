c4234dc56ac28ca347904d2e70818773804d0933
Siequnu <11821606+Siequnu@users.noreply.github.com>
2020-10-14 10:38:00 +0800

    Fix error with new DB init

--- app/__init__.py
+++ app/__init__.py
@@ -22,7 +22,7 @@ from xpinyin import Pinyin
 
 class SQLAlchemy(_BaseSQLAlchemy):
     def apply_pool_defaults(self, app, options):
-        super(SQLAlchemy, self).apply_pool_defaults(self, app, options)
+        super(SQLAlchemy, self).apply_pool_defaults(app, options)
         options["pool_pre_ping"] = True
 db = SQLAlchemy()
 
