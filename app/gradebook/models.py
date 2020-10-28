b2ffb548b278eaaa790599302ee9cf5733572106
Siequnu <11821606+Siequnu@users.noreply.github.com>
2020-10-28 23:07:24 +0800

    Enable saving of percentage objects

--- app/gradebook/models.py
+++ app/gradebook/models.py
@@ -1,9 +1,10 @@
 from app import db
 
-from app.models import Assignment, Upload
+from app.models import Assignment, Upload, User
 from app.assignments.models import AssignmentGrade
 from app.classes.models import get_class_enrollment_from_class_id
 
+import json
 
 # Model to add an entry to the Gradebook
 # Can either be a standalone entry (i.e., title + grade), or connected to an assignment (i.e. linked_assignment -> linked_asignment_grade)
@@ -13,6 +14,40 @@ class GradebookEntry (db.Model):
 	linked_assignment = db.Column(db.Integer, db.ForeignKey('assignment.id'))
 	title = db.Column(db.String(500))
 	date = db.Column(db.Date)
+	combined_weight = db.Column(db.Float)
+
+	def add (self):
+		db.session.add(self)
+		db.session.commit()
+
+
+
+# Model to save the distribution of gradebook percentrages
+'''
+percentages = [
+	{
+		grouped_weight: 30,
+		gradebook_entries: 
+		[
+			{gradebook_entry_id: 1, weight: 45}
+			{gradebook_entry_id: 5, weight: 55}
+		]
+	},
+
+	{
+		grouped_weight: 70,
+		gradebook_entries: 
+		[
+			{gradebook_entry_id: 2, weight: 20}
+			{gradebook_entry_id: 4, weight: 80}
+		]
+	}
+]
+'''
+class GradebookPercentages (db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	turma_id = db.Column(db.Integer, db.ForeignKey('turma.id'))
+	percentages = db.Column(db.String(5000))
 
 	def add (self):
 		db.session.add(self)
@@ -48,44 +83,77 @@ def save_gradebook_grade (student_id, gradebook_entry_id, grade):
 		gradebook_grade.add ()
 
 
+# Save a gradebook percentage object. This will overwrite any existing object
+def save_gradebook_percentage_object (turma_id, percentage_object):
+	existing_percentage_object = GradebookPercentages.query.filter_by(turma_id = turma_id).first()
+
+	if existing_percentage_object is not None:
+		existing_percentage_object.percentages = json.dumps (percentage_object)
+		db.session.commit()
+	
+	else:
+		percentage_object = GradebookPercentages (
+			turma_id = turma_id,
+			percentages = json.dumps (percentage_object)
+		)
+		percentage_object.add()
+
+
 # Get a formatted object containing all assessment criteria for a class
 def get_assessment_criteria_from_class_id (turma_id):
 	assessment_criteria = []
 	
 	# Get all the different assessment points for this class
 	for assessment_type in GradebookEntry.query.filter_by(turma_id = turma_id):
-		
-		# If this is a linked assignment:
-		if assessment_type.linked_assignment != None: 
-		
-			# Get the assignment information
-			assignment = Assignment.query.get(assessment_type.linked_assignment)
-			assessment_criteria.append ({
-				'assessment_type': 'linked_assignment',
-				'assessment': assignment
-			})
-		
-		# Otherwise link the entry
-		else:
-			assessment_criteria.append ({
-				'assessment_type': 'standalone_assessment',
-				'assessment': assessment_type
-			})
-
+		assessment_criteria.append (get_assessment_criteria_object(assessment_type.id))
+	
 	return assessment_criteria
+
+
+# Get a formatted object containing the information for an assessment_type
+def get_assessment_criteria_object (gradebook_entry_id):
+	assessment = GradebookEntry.query.get (gradebook_entry_id)
+	
+	# If this is a linked assignment:
+	if assessment.linked_assignment != None: 
+	
+		# Get the assignment information
+		assignment = Assignment.query.get(assessment.linked_assignment)
+		return {
+			'gradebook_entry': assessment,
+			'assessment_type': 'linked_assignment',
+			'assessment': assignment
+		}
+	
+	# Otherwise link the entry
+	else:
+		return {
+			'gradebook_entry': assessment,
+			'assessment_type': 'standalone_assessment',
+			'assessment': assessment
+		}
+
 
 
 # Function to return a complete class gradebook
 # This will contain an array of students, and each student will have several objects representing the assessment criteria
-def get_class_gradebook (turma_id):
+# Supplying an optional student_id will limit the returned result to that student
+def get_class_gradebook (turma_id, student_id = False):
 	students = []
 	assessment_criteria = get_assessment_criteria_from_class_id (turma_id)
 
-	# Compile a list of all the students in the class
-	for enrollment_ignored, turma_ignored, user in get_class_enrollment_from_class_id (turma_id):
-		user_dict = user.__dict__
+	# If we are supplying a user_id, i.e., single student return mode
+	if student_id :
+		user_dict = User.query.get (student_id).__dict__
 		user_dict['grades'] = []
-		students.append (user_dict) # Convert db Object to a dictionary at this point, so we can add additional criteria
+		students.append (user_dict)
+
+	# Compile a list of all the students in the class
+	else:
+		for enrollment_ignored, turma_ignored, user in get_class_enrollment_from_class_id (turma_id):
+			user_dict = user.__dict__
+			user_dict['grades'] = []
+			students.append (user_dict) # Convert db Object to a dictionary at this point, so we can add additional criteria
 		
 	# Loop through each student and add their assessment criteria
 	for student in students:
@@ -108,6 +176,7 @@ def get_class_gradebook (turma_id):
 				student['grades'].append ({
 					'grade': grade.grade if grade is not None else 'N/A',
 					'is_linked_assignment': True, 
+					'gradebook_entry': assessment['gradebook_entry'],
 					'assessment_id': assessment['assessment'].id,
 					'upload_id': upload.id if upload is not None else None
 				})
@@ -120,6 +189,7 @@ def get_class_gradebook (turma_id):
 				student['grades'].append ({
 					'grade': grade.grade if grade is not None else 'N/A',
 					'is_linked_assignment': False, 
+					'gradebook_entry': assessment['gradebook_entry'],
 					'assessment_id': assessment['assessment'].id
 				})
 						
@@ -132,3 +202,5 @@ def remove_linked_assignment_from_gradebook (turma_id, assignment_id):
 	if gradebook_entry is not None:
 		db.session.delete(gradebook_entry)
 		db.session.commit()
+
+	# Remove this from the class percentage table (how to adjust?)
--- app/gradebook/routes.py
+++ app/gradebook/routes.py
@@ -2,12 +2,14 @@ from flask import render_template, flash, abort, redirect, url_for, jsonify, req
 from flask_login import current_user, login_required
 
 from . import bp
-from .models import GradebookEntry, get_assessment_criteria_from_class_id, save_gradebook_grade, remove_linked_assignment_from_gradebook
+from .models import GradebookEntry, GradebookPercentages, get_assessment_criteria_from_class_id, save_gradebook_grade, remove_linked_assignment_from_gradebook, save_gradebook_percentage_object, get_assessment_criteria_object
 from .forms import AssessmentCriteriaForm
 
 import app.models 
 from app.models import Assignment, Turma
 from app.classes.models import check_if_student_is_in_teachers_class 
+
+import json
 
 
 # Admin API route to save a user's grade
@@ -24,6 +26,61 @@ def api_save_gradebook_grade(student_id, gradebook_entry_id):
 			return jsonify({'success': 'Grade added successfully'})
 			
 		return jsonify({'error': 'This student is not registered in your class.'})
+	abort(403)
+
+
+# Admin API route to save a class gradebook percentage object
+@bp.route("/api/percentages/save/<int:turma_id>", methods=['POST'])
+@login_required
+def api_save_gradebook_percentages_object(turma_id):
+	if app.models.is_admin(current_user.username):
+		# Get the turma
+		turma = Turma.query.get(turma_id)
+		if turma is None: abort (404)
+		
+		# Security check: is this this teacher's class?
+		if app.classes.models.check_if_turma_id_belongs_to_a_teacher (turma_id, current_user.id) is False: 
+			return jsonify({'error': 'You are not registered as managing this class.'})
+
+		percentage_object = request.json.get('percentagesObject')
+		save_gradebook_percentage_object(turma_id, percentage_object)
+		return jsonify({'success': 'Percentage object saved successfully'})
+
+	abort(403)
+
+
+# Admin API route to get a class gradebook percentage object
+@bp.route("/api/percentages/load/<int:turma_id>")
+@login_required
+def api_load_gradebook_percentages_object(turma_id):
+	if app.models.is_admin(current_user.username):
+		# Get the turma
+		turma = Turma.query.get(turma_id)
+		if turma is None: abort (404)
+		
+		# Security check: is this this teacher's class?
+		if app.classes.models.check_if_turma_id_belongs_to_a_teacher (turma_id, current_user.id) is False: 
+			return jsonify({'error': 'You are not registered as managing this class.'})
+
+		existing_percentage_object = GradebookPercentages.query.filter_by(turma_id = turma_id).first()
+
+		if existing_percentage_object is not None:
+			percentages = json.loads (existing_percentage_object.percentages)
+			
+			# Append the assessment information
+			
+			for percentage_object in percentages:
+				for gradebook_entry in percentage_object['gradebook_entries']:
+					assessment_object = get_assessment_criteria_object (gradebook_entry['gradebook_entry_id'])
+					gradebook_entry['assessment_title'] = assessment_object['assessment'].title
+			
+			return jsonify({
+				'turma_id': turma_id,
+				'percentages': percentages
+			})
+		else:
+			return jsonify({'error': 'This class has no existing percentages object.'})
+		
 	abort(403)
 
 
@@ -47,9 +104,28 @@ def gradebook_index(turma_id = False):
 			# Security check: is this this teacher's class?
 			if app.classes.models.check_if_turma_id_belongs_to_a_teacher (turma_id, current_user.id) is False: abort (403)	
 			
-			# Get additional info
-			assignments = app.assignments.models.get_assignments_from_turma_id (turma.id)
+			# Get assessment criteria for this class
 			assessment_criteria = app.gradebook.models.get_assessment_criteria_from_class_id (turma_id)
+			
+			# Get all assignments, and filter through to add metadata
+			assignments = [] 
+			for assignment in app.assignments.models.get_assignments_from_turma_id (turma.id):
+				assignment_dict = assignment.__dict__
+				
+				# Add a flag to indicate if this assignment has already been linked to the class (default is Fale)
+				assignment_dict['already_linked'] = False
+				
+				# Loop through the assessment criteria, and see if we find any matching assignments
+				for assessment_type in assessment_criteria:
+					if assessment_type['assessment_type'] == 'linked_assignment' and assessment_type['assessment'].id == assignment.id:
+						
+						# Mark this assignment as linked
+						assignment_dict['already_linked'] = True
+				
+				# Append this to the assignments array
+				assignments.append (assignment_dict)
+			
+			
 			gradebook = app.gradebook.models.get_class_gradebook (turma.id) # i.e. students and each assignment
 
 			# Get the Assessment Criteria form, which will be used in a modal
@@ -74,6 +150,56 @@ def gradebook_index(turma_id = False):
 				'gradebook_index.html',
 				turma_choices = turma_choices,
 			)
+	
+	# Student view redirects
+	else:
+		return redirect (url_for('gradebook.gradebook_student_index', turma_id = turma_id))
+
+
+# Student gradebook view
+@bp.route("/student")
+@bp.route("/student/<int:turma_id>")
+@login_required
+def gradebook_student_index (turma_id = False):
+	# Make a turma choices dropdown out of the enrollment information
+	turma_choices = []
+	for enrollment, user, turma in app.assignments.models.get_user_enrollment_from_id(current_user.id):
+		turma_choices.append ((turma.id, turma.turma_label))
+
+	# If we are accessing a specific class
+	if turma_id:
+		
+		# Get the turma
+		turma = Turma.query.get(turma_id)
+		if turma is None: abort (404)
+
+		# Check if this student is allowed to access this turma
+		if app.classes.models.check_if_student_is_in_class (current_user.id, turma_id) is not True:
+			abort (403)
+
+		# Get the grade info
+		assessment_criteria = app.gradebook.models.get_assessment_criteria_from_class_id (turma_id) # Assignments+assessment
+		gradebook = app.gradebook.models.get_class_gradebook (turma.id, current_user.id) # i.e. students and each assignment
+
+		# Return the main template, with only the student view variables
+		return render_template (
+				
+				'gradebook_index.html',
+				turma_choices = turma_choices,
+
+				turma = turma,
+				gradebook = gradebook,
+				assessment_criteria = assessment_criteria,
+			)
+
+	# No turma_id, accessing the default home page
+	else:
+	
+		# Get the (student_id only) version of the turma grades
+		return render_template (
+			'gradebook_index.html',
+			turma_choices = turma_choices,
+		)
 
 
 # Route to create a new assessment criteria for a class
@@ -141,7 +267,7 @@ def link_assigment_to_gradebook(turma_id, assignment_id):
 		
 
 # Route to de-link an assignment with a gradebook
-@bp.route('/<int:turma_id>/link/assignment/<int:assignment_id>')
+@bp.route('/<int:turma_id>/unlink/assignment/<int:assignment_id>')
 @login_required
 def remove_linked_assignment(turma_id, assignment_id):
 	if app.models.is_admin(current_user.username):
@@ -153,4 +279,8 @@ def remove_linked_assignment(turma_id, assignment_id):
 
 		# Remove the DB entry
 		remove_linked_assignment_from_gradebook (turma_id, assignment_id)
+
+		flash ('Assignment has been unlinked.', 'success')
+		return redirect (url_for('gradebook.gradebook_index', turma_id = turma_id))
+	abort (403)
 		
